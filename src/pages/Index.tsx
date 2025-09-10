b723970488bd6cb51cfa1e1977734fca7d60d1c7
Ashu <Ashu@sidhart7860gmail.com>
2025-09-11 00:56:22 +0530

    remove the price section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -24,7 +24,7 @@ const Index = () => {
       <SocialProof />
       <FeaturesSection />
       <ProductDemo />
-      <PricingSection />
+      {/* <PricingSection /> */}
       <ResourcesHub />
       <ContactSection />
       <Footer />
